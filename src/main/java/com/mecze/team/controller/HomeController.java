76f715405fc44f21cca9ed1c8bbebebf5539c5cc
Kamil Surma <kamil.surma@asseco.pl>
2025-01-31 22:00:51 +0100

    asdasd

--- src/main/java/com/mecze/team/controller/HomeController.java
+++ src/main/java/com/mecze/team/controller/HomeController.java
@@ -1,16 +1,17 @@
 package com.mecze.team.controller;
 
+import ch.qos.logback.core.model.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/home")
+@Controller
 public class HomeController {
 
     @GetMapping("/home")
-    public String home() {
-        return "home";  // nazwa pliku HTML strony głównej
+    public String home(Model model) {
+        // Możesz dodać dane do modelu, jeśli potrzebujesz
+        return "home"; // Wskazuje na templates/home/home.html
     }
 }
