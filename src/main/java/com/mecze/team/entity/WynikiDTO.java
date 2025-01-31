5a08a25c16949ad1d1a48c5d432301e0c2c7d296
Kamil Surma <kamil.surma@asseco.pl>
2025-01-31 22:25:58 +0100

    asdasd

--- src/main/java/com/mecze/team/entity/WynikiDTO.java
+++ src/main/java/com/mecze/team/entity/WynikiDTO.java
@@ -1,15 +1,32 @@
 package com.mecze.team.entity;
 
-public class WynikiDTO {
+import com.fasterxml.jackson.annotation.JsonProperty;
 
+public class WynikiDTO {
+    @JsonProperty("idDruzyny")
+    private Long idDruzyny;
+
+    @JsonProperty("nazwaDruzyny")
     private String nazwaDruzyny;
+
+    @JsonProperty("meczeRozegrane")
     private int meczeRozegrane;
+
+    @JsonProperty("wygrane")
     private int wygrane;
+
+    @JsonProperty("przegrane")
     private int przegrane;
+
+    @JsonProperty("remisy")
     private int remisy;
+
+    @JsonProperty("punkty")
     private int punkty;
 
-    public WynikiDTO(long l, String nazwaDruzyny, int meczeRozegrane, int wygrane, int przegrane, int remisy, int punkty) {
+    // Konstruktor
+    public WynikiDTO(Long idDruzyny, String nazwaDruzyny, int meczeRozegrane, int wygrane, int przegrane, int remisy, int punkty) {
+        this.idDruzyny = idDruzyny;
         this.nazwaDruzyny = nazwaDruzyny;
         this.meczeRozegrane = meczeRozegrane;
         this.wygrane = wygrane;
@@ -18,52 +35,12 @@ public class WynikiDTO {
         this.punkty = punkty;
     }
 
-
-    public String getNazwaDruzyny() {
-        return nazwaDruzyny;
-    }
-
-    public void setNazwaDruzyny(String nazwaDruzyny) {
-        this.nazwaDruzyny = nazwaDruzyny;
-    }
-
-    public int getMeczeRozegrane() {
-        return meczeRozegrane;
-    }
-
-    public void setMeczeRozegrane(int meczeRozegrane) {
-        this.meczeRozegrane = meczeRozegrane;
-    }
-
-    public int getWygrane() {
-        return wygrane;
-    }
-
-    public void setWygrane(int wygrane) {
-        this.wygrane = wygrane;
-    }
-
-    public int getPrzegrane() {
-        return przegrane;
-    }
-
-    public void setPrzegrane(int przegrane) {
-        this.przegrane = przegrane;
-    }
-
-    public int getRemisy() {
-        return remisy;
-    }
-
-    public void setRemisy(int remisy) {
-        this.remisy = remisy;
-    }
-
-    public int getPunkty() {
-        return punkty;
-    }
-
-    public void setPunkty(int punkty) {
-        this.punkty = punkty;
-    }
+    // Gettery
+    public Long getIdDruzyny() { return idDruzyny; }
+    public String getNazwaDruzyny() { return nazwaDruzyny; }
+    public int getMeczeRozegrane() { return meczeRozegrane; }
+    public int getWygrane() { return wygrane; }
+    public int getPrzegrane() { return przegrane; }
+    public int getRemisy() { return remisy; }
+    public int getPunkty() { return punkty; }
 }
